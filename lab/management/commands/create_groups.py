57cf0087637f18ce739d124dce8b6b97d1b950d0
Verviaus <emre.ozzeybek@gmail.com>
2025-07-29 10:10:16 +0300

    Shifted import to read from xlsx file, also added group and user creation to import function

---
+++ lab/management/commands/create_groups.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+
+
+class Command(BaseCommand):
+    help = 'Create groups'
+
+    def handle(self, *args, **options):
+        if not Group.objects.filter(name='RareIndex Admin').exists():
+            Group.objects.get_or_create(name='RareIndex Admin')
+        if not Group.objects.filter(name='RareIndex User').exists():
+            Group.objects.get_or_create(name='RareIndex User')
+        if not Group.objects.filter(name='RareIndex User(Sensitive Data)').exists():
+            Group.objects.get_or_create(name='RareIndex User(Sensitive Data)')
+        if not Group.objects.filter(name='RareIndex Lab').exists():
+            Group.objects.get_or_create(name='RareIndex Lab')
+        if not Group.objects.filter(name='Collaborator').exists():
+            Group.objects.get_or_create(name='Collaborator')
+        if not Group.objects.filter(name='Pleb').exists():
+            Group.objects.get_or_create(name='Pleb')
+        if not Group.objects.filter(name='Gennext').exists():
+            Group.objects.get_or_create(name='Gennext')
---
+++ lab/management/commands/create_ozbek_users.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.contrib.auth.models import User, Group
+
+class Command(BaseCommand):
+    help = 'Create groups and Ozbek users'
+
+    def handle(self, *args, **options):
+        call_command('create_groups')
+
+        if not User.objects.filter(username='admin').exists():
+            user = User.objects.create_superuser(username='admin', password='admin')
+            admin_group = Group.objects.get(name='RareIndex Admin')
+            user.groups.add(admin_group)
+        if not User.objects.filter(username='pleb').exists():
+            user = User.objects.create_user(username='pleb', password='pleb')
+            pleb_group = Group.objects.get(name='Pleb')
+            user.groups.add(pleb_group)
+        if not User.objects.filter(username='emre.ozzeybek').exists():
+            user = User.objects.create_user(username='emre.ozzeybek', password='Emre', email='emre.ozzeybek@ibg.edu.tr')
+            admin_group = Group.objects.get(name='RareIndex Admin')
+            user.groups.add(admin_group)
+        if not User.objects.filter(username='baris.salman').exists():
+            user = User.objects.create_user(username='baris.salman', password='Baris', email='baris.salman@ibg.edu.tr')
+            admin_group = Group.objects.get(name='RareIndex Admin')
+            user.groups.add(admin_group)
+        if not User.objects.filter(username='ayca.yigit').exists():
+            user = User.objects.create_user(username='ayca.yigit', password='Ayça', email='ayca.yigit@ibg.edu.tr')
+            sensitive_group = Group.objects.get(name='RareIndex User(Sensitive Data)')
+            user.groups.add(sensitive_group)
+        if not User.objects.filter(username='mert.pekerbas').exists():
+            user = User.objects.create_user(username='mert.pekerbas', password='Mert', email='mert.pekerbas@ibg.edu.tr')
+            sensitive_group = Group.objects.get(name='RareIndex User(Sensitive Data)')
+            user.groups.add(sensitive_group)
+        if not User.objects.filter(username='kutay.bulut').exists():
+            user = User.objects.create_user(username='kutay.bulut', password='Kutay', email='kutay.bulut@ibg.edu.tr')
+            user_group = Group.objects.get(name='RareIndex User')
+            user.groups.add(user_group)
+        if not User.objects.filter(username='gennext').exists():
+            user = User.objects.create_user(username='gennext', password='gennext')
+            gennext_group = Group.objects.get(name='Gennext')
+            user.groups.add(gennext_group)
+        if not User.objects.filter(username='ezgi.karaca').exists():
+            user = User.objects.create_user(username='ezgi.karaca', password='Ezgi', email='ezgi.karaca@ibg.edu.tr')
+            collaborator_group = Group.objects.get(name='Collaborator')
+            user.groups.add(collaborator_group)
