4763fc83d1ba6d4917faf886663b949587fd73e2
Verviaus <emre.ozzeybek@gmail.com>
2025-08-29 15:30:52 +0300

    Introduced type filter buttons

--- lab/urls.py
+++ lab/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("get-select-options/", views.get_select_options, name="get_select_options"),
     path("individual/<int:pk>/timeline/", views.individual_timeline, name="individual_timeline"),
     path("get-status-buttons/", views.get_status_buttons, name="get_status_buttons"),
+    path("get-type-buttons/", views.get_type_buttons, name="get_type_buttons"),
     path("get-stats-counts/", plots.get_stats_counts, name="get_stats_counts"),
     path("project/add-individuals/", views.project_add_individuals, name="project_add_individuals"),
     
