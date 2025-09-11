56563c5adb601dcca9bd572d941599c586ecd481
Emre Özzeybek <emre.ozzeybek@gmail.com>
2025-09-11 17:57:22 +0300

    fix urls

--- lab/urls.py
+++ lab/urls.py
@@ -21,10 +21,10 @@ urlpatterns = [
     path("map/", views.map_view, name="map"),
     path("pie-chart/<str:model_name>/<str:attribute_name>/", views.pie_chart_view, name="pie_chart"),
     path("get-select-options/", views.get_select_options, name="get_select_options"),
-    path("individual/<int:pk>/timeline/", timeline.timeline, name="individual_timeline"),
+    path("individual/<int:pk>/timeline/", views.individual_timeline, name="individual_timeline"),
     path("get-status-buttons/", views.get_status_buttons, name="get_status_buttons"),
     path("get-type-buttons/", views.get_type_buttons, name="get_type_buttons"),
-    path("get-stats-counts/", plots.get_stats_counts, name="get_stats_counts"),
+    path("get-stats-counts/", views.get_stats_counts, name="get_stats_counts"),
     path("project/add-individuals/", views.project_add_individuals, name="project_add_individuals"),
     
     # Natural language search routes
